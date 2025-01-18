9ac6d4ab9cf90a26e111f4eb56b382e379a2e53e
habibalmaula <habibalmaula@gmail.com>
2025-01-18 12:56:02 +0700

    Enhance Jenkinsfile and test script for improved CI testing and logging

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -17,4 +17,4 @@ echo 'The following "pnpm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
-pnpm test
+CI=true pnpm test -- --passWithNoTests --testTimeout=10000
