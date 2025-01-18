3518dfd24a7d353b870a53e793ab2b977c6cf08f
habibalmaula <habibalmaula@gmail.com>
2025-01-18 11:59:05 +0700

    Update Jenkinsfile and scripts to use PNPM instead of NPM

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,8 +5,9 @@ echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
+
 set -x
-npm run build
+pnpm run build
 set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
@@ -17,13 +18,14 @@ echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
+
 set -x
-npm start &
+pnpm start &
 sleep 1
 echo $! > .pidfile
 set +x
 
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
+echo '(This is why you specified the "args -p 3000:3000" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
